da8bfda8ab68aa4f9587197318dcd80e3239feb2
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-12-25 20:26:07 -0300

    Implementando rootImport

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,4 +24,11 @@ module.exports = {
     'no-console': ['error', { allow: ['tron'] }],
     'react/jsx-props-no-spreading': 'off',
   },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+    },
+  },
 };
