d322a2a788d07380f625b06195169866bef8f986
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-12-26 21:06:57 -0300

    Utilizando lib unform para facilitar o uso de formularios

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import logo from '~/assets/logo.svg';
 import { Link } from 'react-router-dom';
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" ype="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Acessar</button>
         <Link to="/register">Criar conta gratuita</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -2,19 +2,27 @@ import React from 'react';
 
 import logo from '~/assets/logo.svg';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Nome completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Criar Conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
