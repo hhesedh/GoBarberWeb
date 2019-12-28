ed5c4278bab73e657c11dd8736583b2d67d9a4b5
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-12-27 23:22:32 -0300

    Implementando autenticação

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import logo from '~/assets/logo.svg';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import { signInRequest } from '~/store/modules/auth/actions';
 import * as Yup from 'yup';
+import logo from '~/assets/logo.svg';
 
 const schema = Yup.object().shape({
   email: Yup.string()
@@ -12,8 +14,9 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
   return (
     <>
