4c7c011235f3af2711618c0f12c27f58871d3356
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-12-28 00:01:32 -0300

    Armazenando perfil

--- src/store/modules/rootReducer.js
+++ src/store/modules/rootReducer.js
@@ -1,4 +1,6 @@
 import { combineReducers } from 'redux';
 
 import auth from './auth/reducer';
-export default combineReducers({ auth });
+import user from './user/reducer';
+
+export default combineReducers({ auth, user });
