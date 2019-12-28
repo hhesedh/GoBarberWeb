01d1123aa408a71b1162e2de473c641fab586695
Fábio de Oliveira <fabiodeoliveira07@gmail.com>
2019-12-28 20:23:50 -0300

    Exibindo toasts

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,6 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
+
 import api from '~/services/api';
 import { signInSuccess, signFailure } from './actions';
 import history from '~/services/history';
@@ -15,13 +17,14 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuário não é prestador');
+      toast.error('Usuário não é prestador');
       return;
     }
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
